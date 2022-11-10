9c33b18a59ba6e407f5eb3987c4c94f4ea07daf1
Kristina <qristine-1990@mail.ru>
2022-11-10 15:37:51 +0400

    Домашняя работа

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+/*    
+Console.WriteLine("Примите на вход  число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Примите на вход число b: ");
+int b   = Convert.ToInt32(Console.ReadLine());
+
+int step = a;
+
+ for(int i = 1; i <b; i++)
+    {
+        step = step * a;
+    }
+Console.WriteLine($"A в степени B равно: " + step);
+*/
+
+/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Input a number: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+    int num = i % 10;
+    i = i / 10;
+    sum = sum + num;
+}
+Console.WriteLine("Сумма всех в числе равна: " + sum);
+*/
+
+
